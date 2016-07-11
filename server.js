036c79ee72292b04623e81ecb0f50672312384f3
Emily Jones <efhjones@gmail.com>
2016-07-11 12:31:01 -0600

    Added dependecies

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var mongoose = require('mongoose');
 var http = require('http');
 var bodyParser = require('body-parser');
-var Item = require('./server/itemModel.js');
 
 //initialize the app as an express app
 var app = express();
@@ -20,9 +19,9 @@ app.use(bodyParser.json());
 //handles all requests in order
 
 //express.static handles any wildcard routes
-app.use(express.static(__dirname + '/compiled/src'));
+app.use(express.static(__dirname + '/client'));
 
-mongoose.connect('mongodb://localhost/jennica');
+mongoose.connect('mongodb://localhost/MVP');
 
 app.all('*', function(req, res, next) { 
   res.header('Access-Control-Allow-Origin', '*'); 
@@ -33,54 +32,13 @@ app.all('*', function(req, res, next) { 
 
 app.get('/', function(req, res) {
   res.render('/');
-  res.send("Hello");
+  res.send("Hello, world!");
 });
 
-app.post('/api/list', function(req, res){
-  //add new items to database
-  var name = req.body.name;
-  var quantity = req.body.quantity;
-  var description = req.body.description;
 
-  //new Item made here
-  var item = {
-    name: name,
-    quantity: quantity,
-    description: description
-  };
-  console.log("Server.js line 51", item);
+app.listen(3000);
 
-
-  Item.findOne({ name: name }, function(err, found){
-    if (err){
-      console.log("Bummer ", err);
-      res.send(404);
-    }
-    if (!found){
-      Item.create(item, function(err, item){
-        if (err){
-          console.log("Bummer ", err);
-        } else {
-          console.log("item created ", item);
-
-          res.send(201, item);
-        }
-      });
-    } else {
-      console.log("Item already found");
-      Item.update({ name: name }, item, function(err, item){
-        if (!err){
-          console.log("Updated ", item);
-          res.send(201, item);
-        }
-      });
-    }
-  });
-});
-
-app.listen(1337);
-
-console.log("Server is listening on 1337");
+console.log("Server is listening on 3000");
 
 
 

76900ab3d5445bf0a33921f65359b2e2334df698
Emily Jones <efhjones@gmail.com>
2016-07-11 17:01:45 -0600

    Added jquery, got radio buttons working, submits a post request, but no way to handle it yet! :D

--- client/schemeGeneratorController.js
+++ client/schemeGeneratorController.js
@@ -1,23 +1,33 @@
 angular.module('colorPsychology', [])
 
 
-.controller('schemeGeneratorController', function ($scope, $window, ChooseColors){
+.controller('schemeGeneratorController', function ($scope, $http, $window, ChooseColors){
   console.log("Hello from Controller!");
   $scope.colors = [];
-  $scope.addColor = function(item){
-    $scope.colors.push(item);
+  $scope.color = "";
+  $scope.addColor = function(){
+    $scope.colors.push($scope.color);
+    console.log("Pushed color " + $scope.color + " to colors : " + $scope.colors);
+    if ($scope.colors.length === 4){
+      $scope.submitColors();
+    }
+  }
+  $scope.submitColors = function(){
+    if ($scope.colors.length === 4){
+      ChooseColors.submit($scope.colors);
+    }
   }
 
 })
 
-.factory('ChooseColors', function(){
+.factory('ChooseColors', function($http){
 
   console.log("Choose colors heard the api request!");
 
   var submit = function(data){
-    return $.http({
+    return $http({
       method: 'POST',
-      url: '/api/generate',
+      url: '/traits',
       data: data
     }).then(function (resp){
       resp.send('Thanks!');

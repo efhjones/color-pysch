3120971cef3019aab5d9594a17c1abffb74ba131
Emily Jones <efhjones@gmail.com>
2016-09-19 16:47:03 -0600

    (feat) add router config to app

--- client/app.js
+++ client/app.js
@@ -1,4 +1,14 @@
 angular.module('colorPsych', [
   'colorPsych.services',
   'colorPsych.controllers',
+  'ngRoute'
 ])
+
+
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/', {
+    templateUrl: 'index.html',
+    controller: 'schemeGeneratorController',
+  })
+});
